72873e2080e02d8ead4ccfdf3d1d04c509f4cbbe
jaredlawson <69919616+jaredlawson@users.noreply.github.com>
2025-03-10 21:20:43 +0000

    Deploying to gh-pages from @ jaredlawson/jaredlawson.github.io@aa00e95a6b76fde99c5538a842d4ec2d659f87d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,6 +68,11 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project.html";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project.html";
             },},{
         id: 'social-email',
         title: 'email',
