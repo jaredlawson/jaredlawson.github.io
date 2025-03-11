d665c580eaad2a2b513bf4dc37e1f3308b6df270
jaredlawson <69919616+jaredlawson@users.noreply.github.com>
2025-03-11 21:03:23 +0000

    Deploying to gh-pages from @ jaredlawson/jaredlawson.github.io@fbf7bbcf9524324fc2fb12779042227a93d49244 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "Project details to come.",
+          description: "Some of my work at Vanderbilt",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
